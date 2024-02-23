6b828327cb3e4483eb1957477deaa20cfff4e1db
Matheus Rocha <matheusrochacamarg@gmail.com>
2024-02-22 22:37:56 -0300

    feat: bring poll result via websocket and redis

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
-import z  from "zod";
+import z from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/poll/:pollId", async (request, reply) => {
@@ -18,12 +19,42 @@ export async function getPoll(app: FastifyInstance) {
         options: {
           select: {
             id: true,
-            title: true
-          }
-        }
-      }
+            title: true,
+          },
+        },
+      },
     });
 
-    return reply.send({ poll })
+    if (!poll) {
+      return reply.status(400).send({ message: "Poll not found." });
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+
+    const votes = result.reduce((obj, pollOptionId, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1];
+
+        Object.assign(obj, { [pollOptionId]: Number(score) });
+      }
+
+      return obj;
+    }, {} as Record<string, number>);
+
+    console.log("votes: ", votes);
+
+    return reply.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: option.id in votes ? votes[option.id] : 0,
+          };
+        }),
+      },
+    });
   });
 }
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,21 +1,25 @@
 import fastify from "fastify";
 import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
 import { createPoll } from "./routes/create-poll";
 import { getPolls } from "./routes/get-polls";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify();
 
 app.register(cookie, {
   secret: "polls-app-nlw",
-  hook: "onRequest"
+  hook: "onRequest",
 });
 
+app.register(websocket);
 app.register(createPoll);
 app.register(getPolls);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then((response) => {
   console.log("HTTP server running!", response);
